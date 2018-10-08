7e3c67d5675ead627edef1a9d3666192635d7661
Abhaya Shrestha <ashrestha@mymail.mines.edu>
2018-10-08 02:07:14 -0600

    Added comments to our test class for C13A-1

--- src/tests/testConfigFiles.java
+++ src/tests/testConfigFiles.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 /*
- * */
+ * Part I. Create your own test class (do not modify mine). 
+ * Design your own tests related to loading your configuration files. 
+ * You will also create the skeleton classes so that your tests will compile. 
+ * You will submit failing tests.*/
 class testConfigFiles {
 
 	@Test
