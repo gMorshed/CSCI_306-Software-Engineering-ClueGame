0eac0842354121cff16c8b9a20d302fbdc73a7d1
Abhaya Shrestha <ashrestha@mymail.mines.edu>
2018-10-08 16:28:14 -0600

    Added all the files for 13A-1

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -13,11 +13,31 @@ public class BoardCell {
 	private int row;
 
 	private int column;
+	
+	private char initial; 
 
 	public BoardCell(int row, int column) {
 		super();
 		this.row = row;
 		this.column = column;
+	}
+	
+	/*
+	 * Checks if the BoardCell is a walkway*/
+	public boolean isWalkway() {
+		return false;
+	}
+	
+	/*
+	 * Checks if the BoardCell is a room*/
+	public boolean isRoom() {
+		return false;
+	}
+	
+	/*
+	 * Checks if the BoardCell is a Doorway*/
+	public boolean isDoorway() {
+		return false; 
 	}
 
 }
