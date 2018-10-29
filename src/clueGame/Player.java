5a3c06aa96fbdf4f7ebb1e5303a4ab78f6cc66c1
Abhaya Shrestha <ashrestha@mines.edu>
2018-10-29 16:33:42 -0600

    Made all the specificed new classes from the UML

---
+++ src/clueGame/Player.java
@@ -0,0 +1,27 @@
+package clueGame;
+
+import java.awt.Color;
+import java.io.FileReader;
+
+public class Player {
+
+	private String playerName;
+	
+	private int row ;
+	
+	private int column;
+	
+	private Color color;
+	
+	public Player() {
+		// TODO Auto-generated constructor stub
+		row=0;
+		color=null;
+		column=0;
+		playerName="";
+	}
+	
+	public Card disproveSuggestion(Solution suggestion) { return null;}
+
+
+}
---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+
+public class gameSetupTests {
+
+
+	@Before
+	public void setUp() throws Exception {
+		/**
+		 * This test will set up the board  of variety of cards of each type that 
+		 * will be required for testing
+		 */
+		
+	}
+
+	@Test
+	public void testPeopleLoad() {
+		
+	}
+	
+	
+
+}
