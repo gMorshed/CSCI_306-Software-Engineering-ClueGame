a3e749501356df2cb866986e73a4a04dc18693a7
kirwin lvinodaq <sonkirwin@hotmail.com>
2018-11-05 00:43:58 -0700

    created all successful failing test for test4disproveSuggestion and a stub for handlesuggestion with a small setup but not testing yet

--- src/tests/gameActionTests.java
+++ src/tests/gameActionTests.java
@@ -16,16 +16,19 @@ import clueGame.BoardCell;
 import clueGame.Card;
 import clueGame.CardType;
 import clueGame.ComputerPlayer;
+import clueGame.HumanPlayer;
 import clueGame.Solution;
 
 public class gameActionTests {
 	private static Board board;
-	ArrayList <Card> testDeck;
+	private static ArrayList <Card> testDeck;
 	private static Solution solution,wrongPersonSolution,wrongWeaponSolution,wrongRoomSolution;
 	private static Card cardSolutionPerson, cardSolutionWeapon, cardSolutionRoom,wrongCardSolutionPerson,wrongCardSolutionWeapon,wrongCardSolutionRoom;
+	private static HumanPlayer Steve;
+	private static ComputerPlayer Stark,Natalia,Bruce; //static is one same copy for all instances
 
-	@Before
-	public void setUp() {
+	@BeforeClass
+	public static void setUpBeforeClass() {
 		board = Board.getInstance();
 		board.setConfigFiles("Board_Layout.csv", "ClueRooms.txt");
 		board.setPlayerConfigFile("people.txt");
@@ -38,19 +41,19 @@ public class gameActionTests {
 		testDeck.add(new Card("Natalia Alianovna Romanova", CardType.PERSON));
 		testDeck.add(new Card("Steve Rogers", CardType.PERSON));
 		testDeck.add(new Card("Bruce Banner", CardType.PERSON));
-		
+
 		testDeck.add(new Card("Candlestick", CardType.WEAPON));
 		testDeck.add(new Card("Dumbell", CardType.WEAPON));
 		testDeck.add(new Card("Lead Pipe", CardType.WEAPON));
 		testDeck.add(new Card("Revolver", CardType.WEAPON));
 		testDeck.add(new Card("Rope", CardType.WEAPON));
-		
+
 		testDeck.add(new Card("Study", CardType.ROOM));
 		testDeck.add(new Card("Dining room", CardType.ROOM));
 		testDeck.add(new Card("Billiard Room", CardType.ROOM));
 		testDeck.add(new Card("Presidential Suite", CardType.ROOM));
 		testDeck.add(new Card("Family Room", CardType.ROOM));
-	
+
 	}
 
 	@Test
@@ -76,7 +79,7 @@ public class gameActionTests {
 		assertTrue(loc_15_0); 
 		assertTrue(loc_17_2); 
 		assertTrue(loc_16_1);
-		
+
 		//selecting a cell that has a room in range
 		// we make sure if there is a room in range, the room only visited once
 		player.setRow(2);
@@ -111,7 +114,7 @@ public class gameActionTests {
 		assertTrue(loc_3_6);
 		assertTrue(loc_0_5);
 		assertTrue(countRoomVisited>1); //visit the room once if it has been selected once
-		
+
 	}
 	@Test
 	public void test2CreateSuggestion() {
@@ -123,7 +126,7 @@ public class gameActionTests {
 		boolean natalia = false;
 		boolean steve = false;
 		boolean banner = false;
-		
+
 		//Run 100 calculations
 		for (int i=0; i<100; i++) {
 			Solution suggested = computerPlayer.createSuggestion(testDeck, "Presidential Suite");
@@ -140,7 +143,7 @@ public class gameActionTests {
 			else
 				fail("Invalid target selected");
 		}
-		
+
 		//each card should be picked at least once out of 100!
 		assertTrue(stark);
 		assertTrue(pepper);
@@ -153,7 +156,7 @@ public class gameActionTests {
 		boolean leadPipe = false;
 		boolean rope = false;
 		boolean revolver=false;
-		
+
 		//Run 100 calculations
 		for (int i=0; i<100; i++) {
 			Solution suggestion = computerPlayer.createSuggestion(testDeck, "Study");
@@ -170,28 +173,28 @@ public class gameActionTests {
 			else
 				fail("Invalid target selected");
 		}
-		
+
 		//Ensure each option was selected once
 		assertTrue(candleStick);
 		assertTrue(dumbell);
 		assertTrue(rope);
 		assertTrue(leadPipe);
 		assertTrue(revolver);
-		
-		
+
+
 		//room matches suggestions
 		BoardCell currentCell = board.getCellAt(1,4);
 		Map<Character, String> tempLegend = board.getLegend();
 		String currentRoom = tempLegend.get(currentCell.getInitial());
 		Solution suggestion = computerPlayer.createSuggestion(testDeck, currentRoom);
 		assertEquals(currentRoom, suggestion.getRoom());
-		
+
 		//tests for seen cards not be in returned suggestions
 		//first for weapon cards
 		computerPlayer.clearSeenCard();;
 		computerPlayer.addSeenCard(new Card("Mr. Stark", CardType.PERSON));
 		computerPlayer.addSeenCard(new Card("Candlestick", CardType.WEAPON));
-		
+
 		//Weapon is chosen randomly and not the seen Cards
 		candleStick = false; //candleStick is in the seen cards list, not it shouldn't show up in the suggeested cards
 		dumbell = false;
@@ -214,19 +217,19 @@ public class gameActionTests {
 			else
 				fail("Invalid target selected");
 		}
-		
+
 		//Ensure each option was selected once
 		assertFalse(candleStick);
 		assertTrue(leadPipe);
 		assertTrue(revolver);
 		assertTrue(dumbell);
 		assertTrue(rope);
-		
+
 		//now seen cards tests for Person
 		computerPlayer.clearSeenCard();;
 		computerPlayer.addSeenCard(new Card("Pepper Potts", CardType.PERSON));
 		computerPlayer.addSeenCard(new Card("Lead Pipe", CardType.WEAPON));
-		
+
 		//Person is chosen randomly
 		boolean  potts = false;
 		stark = false;
@@ -249,16 +252,16 @@ public class gameActionTests {
 			else
 				fail("Invalid target selected");
 		}
-		
+
 		//Ensure each option was selected once
 		assertTrue(rogers);
 		assertTrue(stark);
 		assertTrue(natalia);
 		assertFalse(potts);
 	}
-	
+
 	@Test
-	public void test2makeAccusation() {
+	public void test3makeAccusation() {
 		//setting up a solution
 		cardSolutionPerson = new Card("Mr. Stark",CardType.PERSON);
 		wrongCardSolutionPerson = new Card("Steve Rogers",CardType.PERSON);
@@ -266,35 +269,138 @@ public class gameActionTests {
 		wrongCardSolutionWeapon = new Card("Dumbell",CardType.WEAPON);
 		cardSolutionRoom = new Card("Mancave",CardType.ROOM);
 		wrongCardSolutionRoom = new Card("Study",CardType.ROOM);
-		
-	
-		
+
+
+
 		solution = new Solution(cardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
-		
+
 		board.setGameSolution(solution);
+
+		wrongPersonSolution = new Solution(wrongCardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
+		wrongWeaponSolution = new Solution(cardSolutionPerson.getCardName(),wrongCardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
+		wrongRoomSolution = new Solution(cardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),wrongCardSolutionRoom.getCardName());
+
+
+		//solution that is correct
+		assertTrue(board.checkAccusation(solution));
+
+		//solution with wrong person
+		assertFalse(board.checkAccusation(wrongPersonSolution));
+
+		//solution with wrong weapon
+		assertFalse(board.checkAccusation(wrongWeaponSolution));
+
+		//solution with wrong room
+		assertFalse(board.checkAccusation(wrongRoomSolution));
+
+
+
+
+	}
+	@Test
+	public void test4disproveSuggestion() {
+		//Setup: Create a small number of players with known cards
+		Steve = new HumanPlayer(6,2,Color.GRAY, "Steve Rogers" );
+		Stark = new ComputerPlayer( 0, 6, Color.RED, "Mr. Stark");
+		Natalia = new ComputerPlayer(1,6,Color.BLUE, "Natalia Alianovna");
+		
+		Steve.receiveCard(new Card("Steve Rogers", CardType.PERSON));
+		Steve.receiveCard(new Card("Dumbell",CardType.WEAPON));
+		Steve.receiveCard(new Card("Study",CardType.ROOM));
+		Stark.receiveCard(new Card("Mr. Stark", CardType.PERSON));
+		Stark.receiveCard(new Card("Revolver",CardType.WEAPON));
+		Stark.receiveCard(new Card("Mancave",CardType.ROOM));
+		Natalia.receiveCard(new Card("Natalia Alianovna Romanova", CardType.PERSON));
+		Natalia.receiveCard(new Card("Rope", CardType.WEAPON));
+		Natalia.receiveCard(new Card("Dining room", CardType.ROOM));
+		
+		cardSolutionPerson = new Card("Mr. Stark",CardType.PERSON);
+		wrongCardSolutionPerson = new Card("Steve Rogers",CardType.PERSON);
+		cardSolutionWeapon = new Card("Revolver",CardType.WEAPON);
+		wrongCardSolutionWeapon = new Card("Dumbell",CardType.WEAPON);
+		cardSolutionRoom = new Card("Mancave",CardType.ROOM);
+		wrongCardSolutionRoom = new Card("Study",CardType.ROOM);
+
+		Solution oneMatchSuggestionPerson = new Solution(cardSolutionPerson.getCardName(),wrongCardSolutionWeapon.getCardName(),wrongCardSolutionRoom.getCardName());
+		Solution oneMatchSuggestionWeapon = new Solution(wrongCardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),wrongCardSolutionRoom.getCardName());
+		Solution oneMatchSuggestionRoom = new Solution(wrongCardSolutionPerson.getCardName(),wrongCardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
+		//Solution multipleMatchSuggestion = new Solution(cardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
+		Solution noMatchSuggestion = new Solution(wrongCardSolutionPerson.getCardName(),wrongCardSolutionWeapon.getCardName(),wrongCardSolutionRoom.getCardName());
+		
 		
 		wrongPersonSolution = new Solution(wrongCardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
 		wrongWeaponSolution = new Solution(cardSolutionPerson.getCardName(),wrongCardSolutionWeapon.getCardName(),cardSolutionRoom.getCardName());
 		wrongRoomSolution = new Solution(cardSolutionPerson.getCardName(),cardSolutionWeapon.getCardName(),wrongCardSolutionRoom.getCardName());
+
+
+		
+		//If player has only one matching card it should be returned
+		assertEquals(oneMatchSuggestionPerson, Stark.disproveSuggestion(oneMatchSuggestionPerson));
+		assertEquals(oneMatchSuggestionWeapon, Stark.disproveSuggestion(oneMatchSuggestionWeapon));
+		assertEquals(oneMatchSuggestionRoom, Stark.disproveSuggestion(oneMatchSuggestionRoom));
+		
+		//If players has >1 matching card, returned card should be chosen randomly
+		boolean firstTest = false;
+		boolean secondTest = false;
+		boolean thirdTest = false;
+		boolean allPass =false;
+		for (int i=0; i<100; i++) {
+			if (solution.getPerson().equals(Stark.disproveSuggestion(solution).getCardName()))
+				firstTest = true;
+			else if (solution.getPerson().equals(Stark.disproveSuggestion(solution).getCardName()))
+				secondTest = true;
+			else if (solution.getPerson().equals(Stark.disproveSuggestion(solution).getCardName()))
+				thirdTest = true;
+		}
+		if(firstTest && secondTest && thirdTest  ) {
+			allPass = true;
+		}
+		
+		assertTrue(allPass);
+		
+		//If player has no matching cards, null is returned
+		assertEquals(null, Stark.disproveSuggestion(noMatchSuggestion));
+		assertEquals(null, Natalia.disproveSuggestion(noMatchSuggestion));
+		assertEquals(null, Steve.disproveSuggestion(solution));
 		
 		
-		//solution that is correct
-		assertTrue(board.checkAccusation(solution));
-		
-		//solution with wrong person
-		assertFalse(board.checkAccusation(wrongPersonSolution));
-		
-		//solution with wrong weapon
-		assertFalse(board.checkAccusation(wrongWeaponSolution));
-		
-		//solution with wrong room
-		assertFalse(board.checkAccusation(wrongRoomSolution));
-		
-		
-		
-		
+
 	}
-	
-	
+//	@Test
+	public void test5handleSuggestion() {
+		//Setup: Create a small number of players with known cards
+		Steve = new HumanPlayer(6,2,Color.GRAY, "Steve Rogers" );
+		Stark = new ComputerPlayer( 0, 6, Color.RED, "Mr. Stark");
+		Bruce = new ComputerPlayer( 16, 0, Color.GREEN, "Bruce Banner");
+		Natalia = new ComputerPlayer(1,6,Color.BLUE, "Natalia Alianovna");
+		Bruce.receiveCard(new Card("Bruce Banner", CardType.PERSON));
+		Bruce.receiveCard(new Card("Lead Pipe", CardType.WEAPON));
+		Bruce.receiveCard(new Card("Study", CardType.ROOM));
+		Steve.receiveCard(new Card("Steve Rogers", CardType.PERSON));
+		Steve.receiveCard(new Card("Dumbell",CardType.WEAPON));
+		Steve.receiveCard(new Card("Study",CardType.ROOM));
+		Stark.receiveCard(new Card("Mr. Stark", CardType.PERSON));
+		Stark.receiveCard(new Card("Revolver",CardType.WEAPON));
+		Stark.receiveCard(new Card("Mancave",CardType.ROOM));
+		Natalia.receiveCard(new Card("Natalia Alianovna Romanova", CardType.PERSON));
+		Natalia.receiveCard(new Card("Rope", CardType.WEAPON));
+		Natalia.receiveCard(new Card("Dining room", CardType.ROOM));
+		
+
+
+
+		//Suggestion no one can disprove returns null	
+		//Suggestion only accusing player can disprove returns null
+		//Suggestion only human can disprove returns answer (i.e., card that disproves suggestion)
+		//Suggestion only human can disprove, but human is accuser, returns null
+		//Suggestion that two players can disprove, correct player (based on starting with next player in list) returns answer
+		//Suggestion that human and another player can disprove, other player is next in list, ensure other player returns answer
+
+
+	}
+
+
+
+
 }
 
