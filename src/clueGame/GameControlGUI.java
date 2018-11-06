ed2711479ab7a11fe14092a95f5d398dd25c6ad9
Kirwin <kslawren@mines.edu>
2018-11-06 16:18:10 -0700

    Made the game control gui look better

--- src/clueGame/GameControlGUI.java
+++ src/clueGame/GameControlGUI.java
@@ -23,52 +23,64 @@ public class GameControlGUI extends JPanel {
 	{
 		// Create a layout with 2 rows
 		setLayout(new GridLayout(2,0));
-		JPanel panel;
-		panel = createTurnPanel();
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1,2));
+		JPanel intermediatePanel = new JPanel();
+		intermediatePanel.setLayout(new GridLayout(1,3));
+		JButton nextPlayerButton = new JButton("Next Player"); // creates the button
+	    JButton accusationButton = new JButton("Make an accuasation");
+	
 		add(panel);
-		panel = createButtonPanel();
-		add(panel);
-		panel = createRollDiePanel();
-		add(panel);
-		panel = createGuessPanel();
-		add(panel);
-		panel = createGuessResultPanel();
-		add(panel);
+		add(intermediatePanel);
+		panel.add(createTurnPanel());
+		panel.add(nextPlayerButton);
+		panel.add(accusationButton);
+		
+		intermediatePanel.add(createRollDiePanel());
+		intermediatePanel.add(createGuessPanel());
+		intermediatePanel.add(createGuessResultPanel());
 		
 	}
 	//display of the roll of the die
 	 private JPanel createRollDiePanel() {
-		 	JPanel panel = new JPanel();
+		 	JPanel diePanel = new JPanel();
+		 	// diePanel.setLayout(new GridLayout(1,1));
 		 	// Use a grid layout, 1 row, 2 elements (label, text)
-			panel.setLayout(new GridLayout(1, 2));
+		 	diePanel.setLayout(new GridLayout(2, 2));
 		 	JLabel nameLabel = new JLabel("Roll");
-			textField = new JTextField(20);
+			textField = new JTextField(5);
+			nameLabel.setHorizontalAlignment(JLabel.CENTER);
+			nameLabel.setVerticalAlignment(JLabel.CENTER);
+			diePanel.setBorder(new TitledBorder (new EtchedBorder(), "Die")); // titling the border Die
 			textField.setEditable(false); // can't edit this text field
-			panel.add(nameLabel); // add the nameLabel and the textfield in the panel
-			panel.add(textField);
-			panel.setBorder(new TitledBorder (new EtchedBorder(), "Die")); // titling the border Die
-			panel.setPreferredSize(new Dimension(2, 2));
-			return panel;
+			diePanel.add(nameLabel); // add the nameLabel and the textfield in the panel
+			diePanel.add(textField);
+			return diePanel;
 	}
 	 //display of whose turn it is
 	 private JPanel createTurnPanel() {
 		 JPanel panel = new JPanel();
-		 panel.setLayout(new GridLayout(1, 2));
-		 JLabel nameLabel = new JLabel("");
-		 textField = new JTextField();
+		 panel.setLayout(new GridLayout(2, 2));
+		 JLabel nameLabel = new JLabel("Whose turn?");
+		 nameLabel.setHorizontalAlignment(JLabel.CENTER);
+		 nameLabel.setVerticalAlignment(JLabel.CENTER);
+		 textField = new JTextField(20);
 		 textField.setEditable(false);
 		 
 		 panel.add(nameLabel); // this is just going to be a panel rather than having the border as well
 		 panel.add(textField);
-		 panel.setBorder(new TitledBorder(new EtchedBorder(), "Whose turn?"));
-		 return panel;
+		 JPanel panelBackground = new JPanel();
+		 panelBackground.add(panel);
+		 return panelBackground;
 	 }
 	 // display of guesses made by other players
 	 private JPanel createGuessPanel() {
 		 JPanel panel = new JPanel();
 		 panel.setLayout(new GridLayout(2, 2));
 		 JLabel nameLabel = new JLabel("Guess");
-		 textField = new JTextField(20);
+		 textField = new JTextField(5);
+		nameLabel.setHorizontalAlignment(JLabel.CENTER);
+		nameLabel.setVerticalAlignment(JLabel.CENTER);
 		 textField.setEditable(false);
 		 panel.add(nameLabel);
 		 panel.add(textField);
@@ -80,23 +92,15 @@ public class GameControlGUI extends JPanel {
 		 JPanel panel = new JPanel();
 		 panel.setLayout(new GridLayout(2, 2));
 		 JLabel nameLabel = new JLabel("Response");
-		 textField = new JTextField(20);
+		 textField = new JTextField(5);
+		 nameLabel.setHorizontalAlignment(JLabel.CENTER);
+		 nameLabel.setVerticalAlignment(JLabel.CENTER);
 		 textField.setEditable(false);
 		 panel.add(nameLabel);
 		 panel.add(textField);
 		 panel.setBorder(new TitledBorder (new EtchedBorder(), "Guess Result"));
 		 return panel;
 	 }
-	//Creates the buttons Next Player and make an accusation 
-	private JPanel createButtonPanel() {
-		JButton nextPlayerButton = new JButton("Next Player");
-		JButton accusationButton = new JButton("Make an accuasation");
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(2, 2)); // 2 rows 2 elements which are both buttons
-		panel.add(nextPlayerButton);
-		panel.add(accusationButton);
-		return panel;
-	}
 	//main method to display the panel
 	public static void main(String[] args) {
 		// Create a JFrame with all the normal functionality
